60e1052c85ab507202eb827944bf895f7a0c2856
Janblack07 <jandryjacob20@gmail.com>
2025-09-29 17:52:25 -0500

    Apertura de la API-PIZZERIA :fire: :sparkles:

---
+++ src/main/java/com/example/apipizzeria/ApiPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apipizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPizzeriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPizzeriaApplication.class, args);
+    }
+
+}
