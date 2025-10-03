a62022bb63b3f23cbe5a8ef8485d07e4d9a84927
Janblack07 <jandryjacob20@gmail.com>
2025-10-03 17:15:10 -0500

    Fix: Se actualizo la interfaces de usuarios :fire: :hammer:

--- src/main/java/com/example/apipizzeria/Domain/user/repository/UserRepository.java
+++ src/main/java/com/example/apipizzeria/Domain/user/repository/UserRepository.java
@@ -2,12 +2,26 @@ package com.example.apipizzeria.Domain.user.repository;
 
 
 import com.example.apipizzeria.Domain.user.entity.User;
+import com.example.apipizzeria.common.enums.StaffRole;
 import com.example.apipizzeria.common.enums.UserKind;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.*;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     List<User> findByKind(UserKind kind);
+    boolean existsByEmailIgnoreCase(String email);
+    Optional<User> findByEmailIgnoreCase(String email);
+
+    Optional<User> findByIdAndKind(Long id, UserKind kind);
+    Page<User> findByKind(UserKind kind, Pageable pageable);
+    Page<User> findByKindAndActive(UserKind kind, boolean active, Pageable pageable);
+
+    // Para filtrar por rol
+    Page<User> findByKindAndStaffRolesContaining(UserKind kind, StaffRole role, Pageable pageable);
+    Page<User> findByKindAndActiveAndStaffRolesContaining(UserKind kind, boolean active, StaffRole role, Pageable pageable);
 }
