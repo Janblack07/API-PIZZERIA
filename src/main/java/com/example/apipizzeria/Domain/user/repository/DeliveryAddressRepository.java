db0cdf14dbbfb38c005ead91c9c2710f98c8515b
Janblack07 <jandryjacob20@gmail.com>
2025-10-04 21:41:32 -0500

    Feat: Integracion de la interface sobre direcciones del delivery :fire:

--- src/main/java/com/example/apipizzeria/Domain/user/repository/DeliveryAddressRepository.java
+++ src/main/java/com/example/apipizzeria/Domain/user/repository/DeliveryAddressRepository.java
@@ -1,4 +1,20 @@
 package com.example.apipizzeria.Domain.user.repository;
 
-public interface DeliveryAddressRepository {
+import com.example.apipizzeria.Domain.user.entity.Address;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DeliveryAddressRepository extends JpaRepository<Address, Long> {
+
+    Page<Address> findByUserId(Long userId, Pageable pageable);
+
+    Page<Address> findByUserIdAndFavorite(Long userId, boolean favorite, Pageable pageable);
+
+    Optional<Address> findByIdAndUserId(Long id, Long userId);
+
+    List<Address> findByUserId(Long userId); // para limpiar favorite
 }
